0948d526389677fe19abbf65cbd9d30c78b9d784
juulN <juliette.ngan@hotmail.com>
2021-04-14 15:07:08 +0100

    Added calibration function

--- FES_open_loop.m
+++ FES_open_loop.m
@@ -26,101 +26,10 @@ u2 = udpport("LocalPort",22383) %increase by one if error
                                            
 % writeline(bt,strcat("stim on"));
 stimMA = 0;
-
 buffer = 1000; % Buffer for?? 
-
+elecArray = [16, 1, 5]; 
 %% Set safety limits here 
 stimMax = 2; %20mA for aaron
 forceMax = 0.2;
-
-
-%% Start recording for calibration
-% Ensure real-time kernell is installed
-open 'calibrationSim'
-set_param('calibrationSim','SimulationCommand','start')
-
-%% Pulses 
-
-% Start stim for calibration
-for j = 1:3
-i = 1;
-    while (i<=stimMax)
-    force = read(u2,buffer,"double");
-%     b = uint8(abs(a(1))*stimCalibration)
- 
-        stimMA = uint8(i)     
-        if (stimMA <= stimMax && force(buffer) < forceMax)
-%             writeline(bt,strcat("freq ",num2str(200)));
-%             cmd = generate_command([16], [stimMA], [400], elecname);
-%             writeline(bt,cmd)
-%             writeline(bt,strcat("stim ",elecname));
-            write(u1,stimMA,"double","LocalHost",5000);
-        else
-            stimMax = stimMA
-            forceMax = force(buffer)
-            stimMA = 0;
-%             cmd = generate_command([16], [stimMA], [400], elecname);
-%             writeline(bt,cmd)
-%             writeline(bt,strcat("stim ",elecname));
-%             writeline(bt,strcat("stim off"));
-            write(u1,stimMA,"double","LocalHost",5000);
-            break;
-        end
-        pause(3) 
-        stimMA = 0
-%         writeline(bt,strcat("freq ",num2str(200)));
-%         cmd = generate_command([16], [stimMA], [400], elecname);
-%         writeline(bt,cmd)
-%         writeline(bt,strcat("stim ",elecname));
-        write(u1,stimMA,"double","LocalHost",5000);
-        pause(3)
-        i = i+1;
-
-    end
-end
-
-set_param('calibrationSim','SimulationCommand','stop')
-filename = sprintf('calibrationRecording_%s', datestr(now,'mm-dd-yyyy HH-MM'));
-save(filename, 'calibrationRecording') 
-
-%% ID Hammerstein Model from calibration data 
-disp('Identifying model, please wait...')
-
-H_mdl = idnlhw([2, 3, 1], 'pwlinear', 'unitgain', 'Ts', 0.001); 
-
-% Divide data for identification and validation
-% halfIdx = ceil(size(calibrationRecording.time,1)/2);
-% 
-% stimAmpID = calibrationRecording.data(1:halfIdx,2);
-% gripForceID = smoothdata(calibrationRecording.data(1:halfIdx,1), 'SmoothingFactor', 0.03');
-% 
-% stimAmpV = calibrationRecording.data(halfIdx+1:end,2);
-% gripForceV = smoothdata(calibrationRecording.data(halfIdx+1:end,1), 'SmoothingFactor', 0.03');
-
-%
-T1 = load('FES_force_AY_21_03_11_1.mat');
-stimAmpID = T1.ans.data(1:408024,2);
-gripForceID = smoothdata(T1.ans.data(1:408024,1), 'SmoothingFactor', 0.03);
-
-stimAmpV = T1.ans.data(408024:end,2);
-gripForceV = smoothdata(T1.ans.data(408024:end,1), 'SmoothingFactor', 0.03);
-
-%
-mdl = nlhw(iddata(stimAmpID, gripForceID, 0.001), H_mdl); 
-plot(mdl)
-%CHECK WHEN REAL DATA COLLECTED 
-t = 0:0.001:3; 
-gripForceDesired = 0.02 * sin(2*pi*3*t)+0.02;
-stimAmpVestim = sim(mdl, gripForceDesired');
-figure; 
-plot(stimAmpVestim) 
-figure; 
-plot(gripForceDesired)
-
-
-stimAmpVestim = sim(mdl, gripForceDesired');
-figure; 
-plot(stimAmpVestim) 
-figure; 
-plot(gripForceDesired)
-
+h_mdl_struct = idnlhw([2 3 1], 'pwlinear', []); 
+h_mdls = calibration(elecArray, maxStimAmp, maxForce);
