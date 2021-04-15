694e05af6230b8bede0ebc859d92a03f04a6f980
Wacom hrg <Wacom hrg@TABLET-VU04TK7B>
2021-04-15 17:57:16 +0100

    changed electrode n and udp

--- FES_open_loop.m
+++ FES_open_loop.m
@@ -3,23 +3,23 @@
 clc; clear all; close all;
 
 %% Set up Bluetooth connection with Technalia FES device and Init UDP ports
-% bt = bluetooth("0016A474B78F", 1);  %MAC address of device 
-% writeline(bt,"iam DESKTOP");
-% writeline(bt,"battery ? ");
-% writeline(bt,"elec 1 *pads_qty 16");
+bt = bluetooth("0016A474B78F", 1);  %MAC address of device 
+writeline(bt,"iam DESKTOP");
+writeline(bt,"battery ? ");
+writeline(bt,"elec 1 *pads_qty 16");
 
 elecname = "testname1"
 
 % Init UDP ports for simulink comms 
 % matlab and simulink need to be separate instances (same computer)
-u1 = udpport("LocalPort",12383) %increase by one if error
-u2 = udpport("LocalPort",22383) %increase by one if error
+u1 = udpport("LocalPort",12384) %increase by one if error
+u2 = udpport("LocalPort",22385) %increase by one if error
 % 
 
 % Write new stim file (last char of string not transmitted -> add space at end of string)
-% writeline(bt, "sdcard rm default/test/ve5.ptn ")
-% writeline(bt, "sdcard cat > default/test/ve5.ptn ")
-% writeline(bt, "sdcard ed default/test/ve5.ptn CONST CONST R 100 100 3000 ") % 
+writeline(bt, "sdcard rm default/test/ve5.ptn ")
+writeline(bt, "sdcard cat > default/test/ve5.ptn ")
+writeline(bt, "sdcard ed default/test/ve5.ptn CONST CONST R 100 100 3000 ") % 
                              % %amplitude 
                              % %pulsewith 
                              % time(us)(1ms -3000ms)
@@ -30,7 +30,7 @@ stimMA = 0;
 buffer = 1000; % Buffer for?? 
 
 %% Set safety limits here 
-stimMax = 2; %20mA for aaron
+stimMax = 10; %20mA for aaron
 forceMax = 0.2;
 
 
@@ -50,28 +50,28 @@ i = 1;
  
         stimMA = uint8(i)     
         if (stimMA <= stimMax && force(buffer) < forceMax)
-%             writeline(bt,strcat("freq ",num2str(200)));
-%             cmd = generate_command([16], [stimMA], [400], elecname);
-%             writeline(bt,cmd)
-%             writeline(bt,strcat("stim ",elecname));
+            writeline(bt,strcat("freq ",num2str(200)));
+            cmd = generate_command([15], [stimMA], [400], elecname);
+            writeline(bt,cmd)
+            writeline(bt,strcat("stim ",elecname));
             write(u1,stimMA,"double","LocalHost",5000);
         else
             stimMax = stimMA
             forceMax = force(buffer)
             stimMA = 0;
-%             cmd = generate_command([16], [stimMA], [400], elecname);
-%             writeline(bt,cmd)
-%             writeline(bt,strcat("stim ",elecname));
-%             writeline(bt,strcat("stim off"));
+            cmd = generate_command([15], [stimMA], [400], elecname);
+            writeline(bt,cmd)
+            writeline(bt,strcat("stim ",elecname));
+            writeline(bt,strcat("stim off"));
             write(u1,stimMA,"double","LocalHost",5000);
             break;
         end
         pause(3) 
         stimMA = 0
-%         writeline(bt,strcat("freq ",num2str(200)));
-%         cmd = generate_command([16], [stimMA], [400], elecname);
-%         writeline(bt,cmd)
-%         writeline(bt,strcat("stim ",elecname));
+        writeline(bt,strcat("freq ",num2str(200)));
+        cmd = generate_command([15], [stimMA], [400], elecname);
+        writeline(bt,cmd)
+        writeline(bt,strcat("stim ",elecname));
         write(u1,stimMA,"double","LocalHost",5000);
         pause(3)
         i = i+1;
