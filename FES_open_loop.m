b29e909d216efbffa6335fcdfc15bc38773e4695
Wacom hrg <Wacom hrg@TABLET-VU04TK7B>
2021-04-15 14:24:49 +0100

    merged correctly

--- FES_open_loop.m
+++ FES_open_loop.m
@@ -20,118 +20,59 @@ elecname = "testname1"
 writeline(bt, "sdcard rm default/test/ve5.ptn ")
 writeline(bt, "sdcard cat > default/test/ve5.ptn ")
 writeline(bt, "sdcard ed default/test/ve5.ptn CONST CONST R 100 100 3000 ") % 
-                             %amplitude 
-                             %pulsewith 
-                             %time(us)(1ms -3000ms)
+                             % %amplitude 
+                             % %pulsewith 
+                             % time(us)(1ms -3000ms)
                                            
-writeline(bt,strcat("stim on"));
-stimMA = 0;
+% writeline(bt,strcat("stim on"));
 
 %% Set parameters/initialise model
 buffer = 1000; % Buffer for?? 
-
-%% Set safety limits here 
-stimMax = 400; %20mA for aaron
+elecArray = [13, 14, 15]; % Electrode number for each finger 
+stimMax = 2; %20mA for aaron
 forceMax = 0.2;
-stimAmp = 8;
+h_mdl_struct = idnlhw([2 3 1], 'pwlinear', []); 
 
-%% Start recording for calibration
-% Ensure real-time kernell is installed
-open 'calibrationSim'
+%% Identification of model for each electrode
+maxStimAmp = 7;
+maxForce = 0.2; 
 
-%% Pulses 
-elec = [1, 5, 12];
-% Start stim for calibration
-for finger = 1:3
-    set_param('calibrationSim','SimulationCommand','start')
-    for j = 1:3
-    i = 100;
-        while (i<=stimMax)
-        force = read(u2,buffer,"double");
-    %     b = uint8(abs(a(1))*stimCalibration)
+h_mdls = calibration(elecArray, maxStimAmp, maxForce,h_mdl_struct, bt);
 
-            stimMA = uint8(i)     
-            if (stimMA <= stimMax && force(buffer) < forceMax)
-                writeline(bt,strcat("freq ",num2str(200)));
-                cmd = generate_command(elec(finger), [stimAmp], [stimMA], elecname);
-                writeline(bt,cmd)
-                writeline(bt,strcat("stim ",elecname));
-                write(u1,stimMA,"double","LocalHost",5000);
-            else
-                stimMax = stimMA
-                forceMax = force(buffer)
-                stimMA = 0;
-                cmd = generate_command(elec(finger), [stimAmp], [stimMA], elecname);
-                writeline(bt,cmd)
-                writeline(bt,strcat("stim ",elecname));
-                writeline(bt,strcat("stim off"));
-                write(u1,stimMA,"double","LocalHost",5000);
-                break;
-            end
-            pause(3) 
-            stimMA = 0
-            writeline(bt,strcat("freq ",num2str(200)));
-            cmd = generate_command(elec(finger), [stimAmp], [stimMA], elecname);
-            writeline(bt,cmd)
-            writeline(bt,strcat("stim ",elecname));
-            write(u1,stimMA,"double","LocalHost",5000);
-            pause(3)
-            i = i+50;
-
-        end
-    end
-    set_param('calibrationSim','SimulationCommand','stop')
-    filename = sprintf('calibrationRecording_%s', datestr(now,'mm-dd-yyyy HH-MM'));
-    save(filename, 'calibrationRecording') 
-    
-    H_mdl = idnlhw([2, 3, 1], 'pwlinear', 'unitgain', 'Ts', 0.001); 
-
-    % Divide data for identification and validation
-    halfIdx = ceil(size(calibrationRecording.time,1)/2);
-
-    stimAmpID = calibrationRecording.data(1:halfIdx,2);
-    gripForceID = smoothdata(calibrationRecording.data(1:halfIdx,1), 'SmoothingFactor', 0.03');
-
-    stimAmpV = calibrationRecording.data(halfIdx+1:end,2);
-    gripForceV = smoothdata(calibrationRecording.data(halfIdx+1:end,1), 'SmoothingFactor', 0.03');
-    mdl(finger) = nlhw(iddata(stimAmpID, gripForceID, 0.001), H_mdl); 
-
-end 
-
-
-%% ID Hammerstein Model from calibration data 
-disp('Identifying model, please wait...')
-
-
-%
 % T1 = load('FES_force_AY_21_03_11_1.mat');
 % stimAmpID = T1.ans.data(1:408024,2);
 % gripForceID = smoothdata(T1.ans.data(1:408024,1), 'SmoothingFactor', 0.03);
 % 
 % stimAmpV = T1.ans.data(408024:end,2);
 % gripForceV = smoothdata(T1.ans.data(408024:end,1), 'SmoothingFactor', 0.03);
+% 
+% %
+% h_mdls{1} = nlhw(iddata(stimAmpID, gripForceID, 0.001), H_mdl); 
+% h_mdls{2} = nlhw(iddata(stimAmpID, gripForceID, 0.001), H_mdl); 
+% h_mdls{3} = nlhw(iddata(stimAmpID, gripForceID, 0.001), H_mdl); 
+% plot(h_mdls{1})
 
-%
-plot(mdl(1))
-plot(mdl(2))
-plot(mdl(3))
 
-hw_mdl1 = mdl(1);
-hw_mdl2 = mdl(2);
-hw_mdl3 = mdl(3);
+%% Stimulate 
+testname = "testname1"; 
+% Anode is 2, select others 
+elecArray = [16, 1, 5];
+amplitude =6; 
+velecnumber = 5; 
+stimAmp = 8; 
+elecname = "testname1"
 
-%%
+open 'openloopFEScontrollerSim'
+set_param('openloopFEScontrollerSim','SimulationCommand','start')
 
-open 'FEScontrollerSim' 
-set_param('FEScontrollerSim','SimulationCommand','start')
-
-while True
-            FESstim = read(u2,buffer,"double");
-            writeline(bt,strcat("freq ",num2str(200)));
-            cmd = generate_command(elec(finger), [stimAmp], [FESstim], elecname);
-            writeline(bt,cmd)
-            writeline(bt,strcat("stim ",elecname));
-            write(u1,stimMA,"double","LocalHost",5000);
+while true
+    pwFES = read(u2,999,"double");  % ensure buffer is multiple of number of electrodes used 
+    pwFES(end-length(elecArray)+1:end)
+%     writeline(bt,strcat("freq ",num2str(200)));
+    cmd = generate_command(elecArray, [stimAmp stimAmp stimAmp], pwFES(end-length(elecArray)+1:end), elecname);
+%     writeline(bt,cmd)
+%     writeline(bt,strcat("stim ",elecname));
 end
 
+set_param('openloopFEScontrollerSim','SimulationCommand','stop')
 
